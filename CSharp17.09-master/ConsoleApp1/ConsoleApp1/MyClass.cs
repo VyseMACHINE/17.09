090be84b799a5e25762d772570316580a628e538
VyseMACHINE <40603631+VyseMACHINE@users.noreply.github.com>
2018-11-12 20:58:46 +0600

    Add files via upload

---
+++ CSharp17.09-master/ConsoleApp1/ConsoleApp1/MyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MyClass
+    {
+        public int lenght;
+        public int[] arrayInt;
+        public MyClass(int size)
+        {
+            lenght = size;
+            arrayInt = new int[size];
+        }
+        public int this[int index]
+        {
+            set
+            {
+                arrayInt[index] = value*value;
+            }
+            get
+            {
+                return arrayInt[index];
+            }
+        }
+    }
+}
---
+++ CSharp17.09-master/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+            MyClass myClass = new MyClass(size);
+            for (int i = 0; i < myClass.lenght; i++)
+            {
+                myClass[i]=int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < myClass.lenght; i++)
+            {
+                Console.WriteLine(myClass[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
